69515caccca3442e9b436dec7b9957f0d703cd26
Laurent Monin <github@norz.org>
2024-05-29 11:13:18 +0200

    Merge pull request #2502 from zas/parents
    Review and pass proper arguments to parent classes (mainly Qt*)

---
+++ picard/item.py
@@ -0,0 +1,289 @@
+# -*- coding: utf-8 -*-
+#
+# Picard, the next-generation MusicBrainz tagger
+#
+# Copyright (C) 2006-2007 Lukáš Lalinský
+# Copyright (C) 2010, 2018, 2020-2022 Philipp Wolfer
+# Copyright (C) 2011-2012 Michael Wiencek
+# Copyright (C) 2012 Chad Wilson
+# Copyright (C) 2013, 2020-2021, 2023-2024 Laurent Monin
+# Copyright (C) 2014 Sophist-UK
+# Copyright (C) 2021 Gabriel Ferreira
+# Copyright (C) 2021 Petit Minion
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+from PyQt6 import QtCore
+
+from picard import log
+from picard.i18n import ngettext
+from picard.metadata import Metadata
+from picard.util import IgnoreUpdatesContext
+
+
+class Item:
+
+    @property
+    def can_save(self):
+        """Return if this object can be saved."""
+        return False
+
+    @property
+    def can_remove(self):
+        """Return if this object can be removed."""
+        return False
+
+    @property
+    def can_edit_tags(self):
+        """Return if this object supports tag editing."""
+        return False
+
+    @property
+    def can_analyze(self):
+        """Return if this object can be fingerprinted."""
+        return False
+
+    @property
+    def can_autotag(self):
+        """Return if this object can be autotagged."""
+        return False
+
+    @property
+    def can_refresh(self):
+        """Return if this object can be refreshed."""
+        return False
+
+    @property
+    def can_view_info(self):
+        return False
+
+    @property
+    def can_submit(self):
+        """Return True if this object can be submitted to MusicBrainz.org."""
+        return False
+
+    @property
+    def can_show_coverart(self):
+        """Return if this object supports cover art."""
+        return self.can_edit_tags
+
+    @property
+    def can_browser_lookup(self):
+        return True
+
+    @property
+    def is_album_like(self):
+        return False
+
+    @property
+    def can_link_fingerprint(self):
+        """Return True if this item can provide a recording ID for linking to AcoustID."""
+        return False
+
+    def load(self, priority=False, refresh=False):
+        pass
+
+    @property
+    def tracknumber(self):
+        """The track number as an int."""
+        try:
+            return int(self.metadata['tracknumber'])
+        except BaseException:
+            return 0
+
+    @property
+    def discnumber(self):
+        """The disc number as an int."""
+        try:
+            return int(self.metadata['discnumber'])
+        except BaseException:
+            return 0
+
+    @property
+    def errors(self):
+        if not hasattr(self, '_errors'):
+            self._errors = []
+        return self._errors
+
+    def error_append(self, msg):
+        log.error("%r: %s", self, msg)
+        self.errors.append(msg)
+
+    def clear_errors(self):
+        self._errors = []
+
+    @property
+    def _images(self):
+        return self.metadata.images
+
+    def cover_art_description(self):
+        """Return the number of cover art images for display in the UI
+
+        Returns:
+            A string with the cover art image count, or empty string if not applicable
+        """
+        if not self.can_show_coverart:
+            return ''
+
+        return str(len(self._images))
+
+    def cover_art_description_detailed(self):
+        """Return  a detailed text about the images and whether they are the same across
+           all tracks for images in `images` for display in the UI
+
+        Returns:
+            A string explaining the cover art image count.
+        """
+        if not self.can_show_coverart:
+            return ''
+
+        number_of_images = len(self._images)
+        if getattr(self, 'has_common_images', True):
+            return ngettext("%i image", "%i images",
+                            number_of_images) % number_of_images
+        else:
+            return ngettext("%i image not in all tracks", "%i different images among tracks",
+                            number_of_images) % number_of_images
+
+
+class MetadataItem(Item):
+    metadata_images_changed = QtCore.pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.metadata = Metadata()
+        self.orig_metadata = Metadata()
+        self.update_children_metadata_attrs = {}
+        self.iter_children_items_metadata_ignore_attrs = {}
+        self.suspend_metadata_images_update = IgnoreUpdatesContext()
+
+    def update_metadata_images(self):
+        if not self.suspend_metadata_images_update and self.can_show_coverart:
+            if self.update_metadata_images_from_children():
+                self.metadata_images_changed.emit()
+
+    def keep_original_images(self):
+        with self.suspend_metadata_images_update:
+            for file in list(self.files):
+                if file.can_show_coverart:
+                    file.keep_original_images()
+        self.update_metadata_images()
+
+    def children_metadata_items(self):
+        """Yield MetadataItems that are children of the current object"""
+
+    def iter_children_items_metadata(self, metadata_attr):
+        for s in self.children_metadata_items():
+            if metadata_attr in s.iter_children_items_metadata_ignore_attrs:
+                continue
+            yield getattr(s, metadata_attr)
+
+    @staticmethod
+    def get_sources_metadata_images(sources_metadata):
+        images = set()
+        for s in sources_metadata:
+            images = images.union(s.images)
+        return images
+
+    def remove_metadata_images_from_children(self, removed_sources):
+        """Remove the images in the metadata of `removed_sources` from the metadata.
+
+        Args:
+            removed_sources: List of child objects (`Track` or `File`) which's metadata images should be removed from
+        """
+        changed = False
+
+        for metadata_attr in self.update_children_metadata_attrs:
+            removed_images = self.get_sources_metadata_images(getattr(s, metadata_attr) for s in removed_sources)
+            sources_metadata = list(self.iter_children_items_metadata(metadata_attr))
+            metadata = getattr(self, metadata_attr)
+            changed |= metadata.remove_images(sources_metadata, removed_images)
+
+        return changed
+
+    def add_metadata_images_from_children(self, added_sources):
+        """Add the images in the metadata of `added_sources` to the metadata.
+
+        Args:
+            added_sources: List of child objects (`Track` or `File`) which's metadata images should be added to current object
+        """
+        changed = False
+
+        for metadata_attr in self.update_children_metadata_attrs:
+            added_images = self.get_sources_metadata_images(getattr(s, metadata_attr) for s in added_sources)
+            metadata = getattr(self, metadata_attr)
+            changed |= metadata.add_images(added_images)
+
+        return changed
+
+    def update_metadata_images_from_children(self):
+        """Update the metadata images of the current object based on its children.
+
+        Based on the type of the current object, this will update `self.metadata.images` to
+        represent the metadata images of all children (`Track` or `File` objects).
+
+        This method will iterate over all children and completely rebuild
+        `self.metadata.images`. Whenever possible the more specific functions
+        `add_metadata_images_from_children` or `remove_metadata_images_from_children` should be used.
+
+        Returns:
+            bool: True, if images where changed, False otherwise
+        """
+        from picard.util.imagelist import ImageList
+
+        class ImageListState:
+            def __init__(self):
+                self.images = {}
+                self.has_common_images = True
+                self.first_obj = True
+
+            def process_images(self, src_obj_metadata):
+                src_dict = src_obj_metadata.images.hash_dict()
+                prev_len = len(self.images)
+                self.images.update(src_dict)
+                if len(self.images) != prev_len:
+                    if not self.first_obj:
+                        self.has_common_images = False
+                if self.first_obj:
+                    self.first_obj = False
+
+        changed = False
+
+        for metadata_attr in self.update_children_metadata_attrs:
+            state = ImageListState()
+            for src_obj_metadata in self.iter_children_items_metadata(metadata_attr):
+                state.process_images(src_obj_metadata)
+
+            updated_images = ImageList(state.images.values())
+            metadata = getattr(self, metadata_attr)
+            changed |= set(updated_images.hash_dict()) != set(metadata.images.hash_dict())
+            metadata.images = updated_images
+            metadata.has_common_images = state.has_common_images
+
+        return changed
+
+
+class FileListItem(MetadataItem):
+
+    def __init__(self, files=None):
+        super().__init__()
+        self.files = files or []
+        self.update_children_metadata_attrs = {'metadata', 'orig_metadata'}
+
+    def iterfiles(self, save=False):
+        yield from self.files
+
+    def children_metadata_items(self):
+        yield from self.files
---
+++ picard/ui/util.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+#
+# Picard, the next-generation MusicBrainz tagger
+#
+# Copyright (C) 2007 Lukáš Lalinský
+# Copyright (C) 2013-2015, 2018-2024 Laurent Monin
+# Copyright (C) 2016-2017 Sambhav Kothari
+# Copyright (C) 2017 Frederik “Freso” S. Olesen
+# Copyright (C) 2017 Sophist-UK
+# Copyright (C) 2018 Vishal Choudhary
+# Copyright (C) 2019, 2021-2022, 2024 Philipp Wolfer
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+from html import escape as html_escape
+
+from PyQt6 import (
+    QtCore,
+    QtGui,
+    QtWidgets,
+)
+
+from picard.config import get_config
+from picard.const.sys import (
+    IS_LINUX,
+    IS_MACOS,
+    IS_WIN,
+)
+from picard.i18n import (
+    N_,
+    gettext as _,
+)
+from picard.util import find_existing_path
+
+from picard.ui.enums import MainAction
+
+
+class StandardButton(QtWidgets.QPushButton):
+
+    OK = 0
+    CANCEL = 1
+    HELP = 2
+    CLOSE = 4
+
+    __types = {
+        OK: (N_("&Ok"), 'SP_DialogOkButton'),
+        CANCEL: (N_("&Cancel"), 'SP_DialogCancelButton'),
+        HELP: (N_("&Help"), 'SP_DialogHelpButton'),
+        CLOSE: (N_("Clos&e"), 'SP_DialogCloseButton'),
+    }
+
+    def __init__(self, btntype):
+        label = _(self.__types[btntype][0])
+        args = [label]
+        if not IS_WIN and not IS_MACOS:
+            iconname = self.__types[btntype][1]
+            if hasattr(QtWidgets.QStyle, iconname):
+                icon = QtCore.QCoreApplication.instance().style().standardIcon(getattr(QtWidgets.QStyle, iconname))
+                args = [icon, label]
+        super().__init__(*args)
+
+
+def find_starting_directory():
+    config = get_config()
+    if config.setting['starting_directory']:
+        path = config.setting['starting_directory_path']
+    else:
+        path = config.persist['current_directory'] or QtCore.QDir.homePath()
+    return find_existing_path(path)
+
+
+class MultiDirsSelectDialog(QtWidgets.QFileDialog):
+
+    """Custom file selection dialog which allows the selection
+    of multiple directories.
+    Depending on the platform, dialog may fallback on non-native.
+    """
+
+    def __init__(self, parent=None, caption="", directory="", filter=""):
+        super().__init__(parent=parent, caption=caption, directory=directory, filter=filter)
+        self.setFileMode(QtWidgets.QFileDialog.FileMode.Directory)
+        self.setOption(QtWidgets.QFileDialog.Option.ShowDirsOnly)
+        # The native dialog doesn't allow selecting >1 directory
+        self.setOption(QtWidgets.QFileDialog.Option.DontUseNativeDialog)
+        for view in self.findChildren((QtWidgets.QListView, QtWidgets.QTreeView)):
+            if isinstance(view.model(), QtGui.QFileSystemModel):
+                view.setSelectionMode(QtWidgets.QAbstractItemView.SelectionMode.ExtendedSelection)
+        self.set_sidebar_locations()
+
+    def set_sidebar_locations(self):
+        # Allow access to all mounted drives in the sidebar
+        root_volume = "/"
+        volume_paths = []
+        for volume in QtCore.QStorageInfo.mountedVolumes():
+            if volume.isValid() and volume.isReady():
+                path = volume.rootPath()
+                if volume.isRoot():
+                    root_volume = path
+                else:
+                    if not IS_LINUX or (path.startswith("/media/") or path.startswith("/mnt/")):
+                        volume_paths.append(path)
+        paths = [
+            root_volume,
+            QtCore.QDir.homePath(),
+            QtCore.QStandardPaths.writableLocation(QtCore.QStandardPaths.StandardLocation.MusicLocation),
+        ]
+        self.setSidebarUrls(QtCore.QUrl.fromLocalFile(p) for p in paths + sorted(volume_paths) if p)
+
+
+def qlistwidget_items(qlistwidget):
+    """Yield all items from a QListWidget"""
+    for i in range(qlistwidget.count()):
+        yield qlistwidget.item(i)
+
+
+def changes_require_restart_warning(parent, warnings=None, notes=None):
+    """Display a warning dialog about modified options requiring a restart"""
+    if not warnings:
+        return
+    text = '<p><ul>'
+    for warning in warnings:
+        text += '<li>' + html_escape(warning) + '</li>'
+    text += "</ul></p>"
+    if notes:
+        for note in notes:
+            text += "<p><em>" + html_escape(note) + "</em></p>"
+    text += "<p><strong>" + _("You have to restart Picard for the changes to take effect.") + "</strong></p>"
+    QtWidgets.QMessageBox.warning(
+        parent,
+        _("Changes only applied on restart"),
+        text
+    )
+
+
+def menu_builder(menu, main_actions, *args):
+    """Adds each argument to menu, depending on their type"""
+    for arg in args:
+        if arg == '-':
+            menu.addSeparator()
+        elif isinstance(arg, QtWidgets.QMenu):
+            menu.addMenu(arg)
+        elif isinstance(arg, MainAction) and main_actions[arg]:
+            menu.addAction(main_actions[arg])
+        elif isinstance(arg, QtWidgets.QWidgetAction):
+            menu.addAction(arg)
